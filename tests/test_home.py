610f05a49db481815b4015e21df71b4e18f95b6a
Sergei Mikurov <454004@mail.ru>
2024-05-20 02:43:34 +0500

    Add test_home_page_links_to_site_pages

--- tests/test_home.py
+++ tests/test_home.py
@@ -18,3 +18,18 @@ def run_around_tests(page: Page) -> Generator[None, None, None]:
 
 def test_title(page: Page) -> None:
     expect(page).to_have_title('Домашняя страница')
+
+
+def test_home_page_links_to_site_pages(page: Page) -> None:
+    links = {
+        'Упражнение "Изучаем слова"': 'task/english-translate-choice/',
+        'Добавить слово в словарь': 'english/word/create/',
+    }
+    for link_name, link_href in links.items():
+        link = page.locator('a', has_text=link_name)
+        expect(link).to_have_text(link_name)
+
+        link.click()
+        expect(page).to_have_title(link_name)
+        expect(page.get_by_test_id('headline')).to_have_text(link_name)
+        page.goto(HOST)
