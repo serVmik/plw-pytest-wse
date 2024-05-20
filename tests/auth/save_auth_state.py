1acfd40aead8a0b38abfdeac84547a2d15387fbf
Sergei Mikurov <454004@mail.ru>
2024-05-21 02:49:47 +0500

    Fix host

--- tests/auth/save_auth_state.py
+++ tests/auth/save_auth_state.py
@@ -1,11 +1,13 @@
 import os
+from urllib.parse import urljoin
 
 from dotenv import load_dotenv
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
 
 load_dotenv()
 
-URL = 'http://localhost:8000/users/login/'
+HOST = os.getenv('HOST')
+LOGIN_PATH = 'users/login/'
 TEST_USERNAME = os.getenv('TEST_USERNAME')
 TEST_USER_PASSWORD = os.getenv('TEST_USER_PASSWORD')
 PATH_STATE = '/home/dev/projects/plw-pytest-wse/tests/auth/state.json'
@@ -15,13 +17,20 @@ def get_state(playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    page.goto(URL)
+    page.goto(urljoin(HOST, LOGIN_PATH))
 
     page.locator('#id_username').fill(TEST_USERNAME)
     page.locator('#id_password').fill(TEST_USER_PASSWORD)
     page.get_by_test_id('login-button').click()
 
     page.context.storage_state(path=PATH_STATE)
+
+    try:
+        expect(page.get_by_text('Выйти')).to_be_visible()
+    except AssertionError:
+        print('Fatal')
+    else:
+        print('Success')
 
 
 if __name__ == '__main__':
